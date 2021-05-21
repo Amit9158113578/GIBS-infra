82c004abf2bea0d2df9cd2cfbcb29e4931ea8a04
Neerajkumar <neerajg@comsenseconsulting.com>
2021-05-21 16:53:48 +0530

    update properties api

--- role_user_migration/run_api.sh
+++ role_user_migration/run_api.sh
@@ -1,2 +1,3 @@
 cd /etc/role_user_migration
 mvn clean install
+java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.0.jar --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/application-prod.properties"

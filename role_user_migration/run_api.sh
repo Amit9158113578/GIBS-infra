da440ced4138b56ec92e60f753bdf334ceb1bd44
Neerajkumar <neerajg@comsenseconsulting.com>
2021-05-21 17:03:42 +0530

    Update run_api.sh

--- role_user_migration/run_api.sh
+++ role_user_migration/run_api.sh
@@ -1,3 +1,3 @@
 cd /etc/role_user_migration
 mvn clean install
-java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.0.jar --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/application-prod.properties"
+java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.0.jar elastic GBIS@Elastic@321 --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/application-prod.properties"

2437149fc7a1382ec8a36f527c3f6d4d8871a5a3
Neerajkumar <neerajg@comsenseconsulting.com>
2021-05-30 19:31:52 +0530

    refactored
    1. change variables to upper case
    2. re-arranged scripts and templates for easy of use
    3. moved GGC logstash pipelines to GGC file upload project
    4. renamed jobs and scripts for ease of use

--- role_user_migration/run_api.sh
+++ role_user_migration/run_api.sh
@@ -1,3 +1,3 @@
 cd /etc/role_user_migration
 mvn clean install
-java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.0.jar ${elasticsearch_username} ${elasticsearch_password} --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/${property_file}"
+java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.0.jar ${ELASTICSEARCH_USERNAME} ${ELASTICSEARCH_PASSWORD} --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/${PROPERTY_FILE}"

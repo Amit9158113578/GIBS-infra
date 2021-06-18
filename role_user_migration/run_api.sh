a7c00bb9bc16a36f8505981e33c43c9bc674f1cb
Neerajkumar <neerajg@comsenseconsulting.com>
2021-06-18 18:05:17 +0530

    update to migration api
    - parameterized space ID
    - added evnsubt to migration template
    - renamed roles_user_api to migration

--- role_user_migration/run_api.sh
+++
@@ -1,3 +0,0 @@
-cd /etc/role_user_migration
-mvn clean install
-java -jar /etc/role_user_migration/target/RwandaCreateUser-v1.1.jar ${ELASTICSEARCH_USERNAME} ${ELASTICSEARCH_PASSWORD} --spring.config.name=application --spring.config.location="/etc/role_user_migration/src/main/resources/${PROPERTY_FILE}"

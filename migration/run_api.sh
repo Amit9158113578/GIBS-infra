3d6de3e53615eff2475cd72dbc2c19833e287065
Neerajkumar <neerajg@comsenseconsulting.com>
2021-06-18 18:13:10 +0530

    Update run_api.sh

--- migration/run_api.sh
+++ migration/run_api.sh
@@ -1,3 +1,3 @@
-cd /etc/role_user_migration
+cd /etc/migration
 mvn clean install
 java -jar /etc/migration/target/RwandaCreateUser-v1.1.jar ${ELASTICSEARCH_USERNAME} ${ELASTICSEARCH_PASSWORD} --spring.config.name=application --spring.config.location="/etc/migration/src/main/resources/${PROPERTY_FILE}.properties"

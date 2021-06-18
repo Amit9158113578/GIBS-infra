eb2d204fa040f05b2ad7bda55292030ff7884f47
Neerajkumar Gogavale <ngogavale@aptude.com>
2021-06-18 16:06:45 +0200

    Update temp.sh

--- temp/temp.sh
+++ temp/temp.sh
@@ -1,1 +1,1 @@
-curl -u elastic:GBIS@Elastic@321 -XGET https://10.10.73.15:5601/api/spaces/space/development--playground
+curl -k -u elastic:GBIS@Elastic@321 -XGET https://10.10.73.15:5601/api/spaces/space/development--playground

f40aeb3dd208e5247834a2b5f1450c52abe4d350
Omkar <odhapte@aptude.com>
2021-06-18 18:20:17 +0530

    - Build 18_Jun_2021_v1.2

--- migration/run_api.sh
+++ migration/run_api.sh
@@ -1,3 +1,3 @@
 cd /etc/migration
 mvn clean install
-java -jar /etc/migration/target/RwandaCreateUser-v1.1.jar ${ELASTICSEARCH_USERNAME} ${ELASTICSEARCH_PASSWORD} --spring.config.name=application --spring.config.location="/etc/migration/src/main/resources/${PROPERTY_FILE}.properties"
+java -jar /etc/migration/target/RwandaCreateUser-v1.2.jar ${ELASTICSEARCH_USERNAME} ${ELASTICSEARCH_PASSWORD} --spring.config.name=application --spring.config.location="/etc/migration/src/main/resources/${PROPERTY_FILE}.properties"

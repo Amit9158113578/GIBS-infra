6ffe9dd3acd693b7e70fedfce02dfa47cf87ce25
Omkar <odhapte@aptude.com>
2021-06-21 15:13:53 +0530

    - Build 21_Jun_2021_v1.4

--- migration/src/main/java/com/rwanda/model/RequestForObject.java
+++ migration/src/main/java/com/rwanda/model/RequestForObject.java
@@ -2,6 +2,7 @@ package com.rwanda.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.List;
 
 @JsonInclude(Include.NON_NULL)
 public class RequestForObject {

a8a4bcd543130032a358451d95ebbfe4ea9946f2
Neerajkumar G <ngogavale@aptude.com>
2022-03-30 14:19:12 +0530

    sync branches

--- base_setup/base_setup.sh
+++ base_setup/base_setup.sh
@@ -15,31 +15,17 @@ fi
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt update -y
 sudo apt install ansible -y 
-sudo apt-get install python-jmespath zip unzip git -y
+#sudo apt-get install python-jmespath zip unzip git -y
 
 
 #Making ansible cfg file
 echo '[defaults]
+log_path = ./ansible.log
 private_key_file = /root/.ssh/id_rsa
 remote_user = root
+interpreter_python = /usr/bin/python3
 host_key_checking=False' | sudo tee  /etc/ansible/ansible.cfg
 
-#Making /etc/hosts entries
-
-# grep "$node1 node1 
-# $node2 node2 
-# $node3 node3
-# 10.10.74.133 www.gov.rw" /etc/hosts
-
-# if [ $? != 0 ]
-# then
-# echo "$node1 node1 
-# $node2 node2 
-# $node3 node3
-# 10.10.74.133 www.gov.rw"| sudo tee -a /etc/hosts
-# else
-# echo "hosts entries already present"
-# fi
 
 grep "$node1 node1" /etc/hosts
 
